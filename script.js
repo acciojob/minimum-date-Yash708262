1cc81a974efbe394687b0649a39b6f72e4f6299b
Yash708262 <yash708262@gmail.com>
2025-08-27 20:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function minDate(dates) {
-  //write you code here
+  let earliestdate = date[0];
+    for(let i = 1; i < dates.length;i++){
+        if(dates[i] < earliestdate){
+            earliestdate = dates[i];
+        }
+    }
 }
 
 // Do not change the code
@@ -37,4 +42,4 @@ var dates = [
   "2023/03/30",
 ];
 
-alert(minDate(dates));
+// alert(minDate(dates));
